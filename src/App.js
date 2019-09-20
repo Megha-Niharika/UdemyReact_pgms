2f06b812f0bce7719a0dbdecc9cd112cb211f415
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-20 18:29:06 +0530

    update1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import Person from "./Person/Person"
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>am a react app</h1>
-  //   </div>
-  // );
-  return React.createElement('div',null, React.createElement('h1',null,'Hi, I am Rea!!'));  
+  return (
+    <div className="App">
+      <h1>am a react app</h1>
+      <Person/>
+    </div>
+  );
+ // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I am Rea!!'));  
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+const person = () => {
+     return <p>aaaaaaaaaaa</p>
+
+};
+
+export default person;
