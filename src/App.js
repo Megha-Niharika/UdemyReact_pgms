0075d0190cc1f95433cc51573409881f6302084a
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-20 17:04:37 +0530

    udemy pgm basic

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  // return (
+  //   <div className="App">
+  //     <h1>am a react app</h1>
+  //   </div>
+  // );
+  return React.createElement('div',null, React.createElement('h1',null,'Hi, I am Rea!!'));  
+}
+
+export default App;
