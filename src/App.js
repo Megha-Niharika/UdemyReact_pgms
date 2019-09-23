9399b476b9c4fb9c43da6abd3cb545029bc3f85a
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-23 12:27:02 +0530

    State property and event handling using button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from "./Person/Person"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>am a react app</h1>
-      <Person name="Abhay" age="28"/>
-        <Person name="vivian" age="26">My Everything</Person>
-        <Person name="dsena" age="29" />
-    </div>
-  );
- // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I am Rea!!'));  
+class App extends Component {
+state = {
+  persons : [
+    
+     {name: "Abhay" , age:"28"} ,
+     {name: "vivan" , age:"29"},
+      {name :"dsena" , age:"26"}
+    
+  ]
 }
+
+switchNameHandler = () =>
+{
+ this.setState({
+   persons : [
+    
+  {name: "niha" , age:"28"} ,
+  {name: "vivan" , age:"29"},
+   {name :"dsena" , age:"24"}
+ 
+]})
+}
+  render() {
+    return (
+      <div className="App">
+        <h1>am a react app</h1>
+        <p>Its working!!!</p>
+        <button onClick={this.switchNameHandler}>switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+ // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I am Rea!!'));  
+
 
 export default App;
