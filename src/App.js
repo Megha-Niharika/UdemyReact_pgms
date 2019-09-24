3ac0a3571574ff4cf3c54b9abcbd3374f3884677
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-24 11:51:34 +0530

    rendering content by condition

--- src/App.js
+++ src/App.js
@@ -1,61 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Person from "./Person/Person"
 import './App.css';
 import './Person/Person.css';
 
 class App extends Component {
-state = {
-  persons : [
-    
-     {name: "Abhay" , age:"28"} ,
-     {name: "vivan" , age:"29"},
-      {name :"dsena" , age:"26"}
-    
-  ],
-  otherState: "some other value",
-  showPersons: false
-}
+  state = {
+    persons: [
 
-switchNameHandler = (newName) =>
-{
- this.setState({
-   persons : [
-    
-  {name: newName , age:"28"} ,
-  {name: "vivan" , age:"29"},
-   {name :"dsena" , age:"24"}
- 
-]})
-}
+      { name: "Abhay", age: "28" },
+      { name: "vivan", age: "29" },
+      { name: "dsena", age: "26" }
 
-nameChangedHandler =(event) =>
-{
-  this.setState({
-    persons : [
-     
-   {name: "mac" , age:"28"} ,
-   {name: event.target.value, age:"29"},
-    {name :"megha", age:"24"}
-  
- ]})
-}
+    ],
+    otherState: "some other value",
+    showPersons: false
+  }
 
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
 
+        { name: newName, age: "28" },
+        { name: "vivan", age: "29" },
+        { name: "dsena", age: "24" }
 
-togglePersonsHandler =() =>
-{
-const doesShow = this.state.showPersons;
-this.setState({showPersons: ! doesShow});
+      ]
+    })
+  }
 
-}
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+
+        { name: "mac", age: "28" },
+        { name: event.target.value, age: "29" },
+        { name: "megha", age: "24" }
+
+      ]
+    })
+  }
+                                                                                       
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+
+  }
   render() {
- const style ={
-  backgroundColor :"white",
-  font: "inherit",
-  border: "1px solid blue",
-  padding: "8px",
-  cursor :"pointer"
- };
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
 
 
     return (
@@ -63,36 +60,36 @@ this.setState({showPersons: ! doesShow});
         <h1>am a react app</h1>
         <p>Its working!!!</p>
         <button
-        
-        style ={style}
-      onClick={this.togglePersonsHandler}>Switch Name</button>
-         { this.state.showPersons === true ?  
+
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+        {this.state.showPersons === true?
           <div>
 
-         <Person 
-        name={this.state.persons[0].name}
-         age={this.state.persons[0].age}/>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this,"ayan")}
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "ayan")}
 
-        changed ={this.nameChangedHandler}> my hobbies 
+              changed={this.nameChangedHandler}> my hobbies
         </Person>
-        <Person
-        name={this.state.persons[2].name}
-         age={this.state.persons[2].age}
-         click={this.switchNameHandler.bind(this,"aryn")}
-        />
-      </div> : null
-         }
-            </div>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              click={this.switchNameHandler.bind(this, "aryn")}
+            />
+          </div> :null
+        }
+      </div>
 
-        
+
     );
   }
 }
- // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I am Rea!!'));  
+// return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I am Rea!!'));  
 
 
 export default App;
